7b522293914cc9154979bfdcd7def5877f0fceea
aroberge66 <alecroberge6@gmail.com>
2023-07-25 10:41:44 -0600

    made data work

--- heatmap/heatmap.js
+++ heatmap/heatmap.js
@@ -1,67 +1,99 @@
 // JavaScript Document
 
-
-
-const svg= d3.select("svg")
+const svg = d3.select('svg')
 
 svg
-	.attr("width", 800)
-	.attr("height", data.length * 150)
+  .attr('width', 800)
+  .attr('height', data.length * 150)
 
-const dataPoints=svg
-	.selectAll("g.data-point")
-	.data(data)
-	.enter()
-	.append("g")
-	.attr("class", "data-point")
-	.attr("transform", (d, i) => {return `translate{0, $(i * 150})`})
-
-dataPoints
-	.append("text")
-	.attr("x", 175)
-	.attr("y", 70)
-	.attr("class", "city")
-	.text((d, i) => {return d.city})
-	.attr("transform", (d, i) => {
-	const x = (i )
-	const y = Math.floor(i *2) * 120	
-	return `translate(${x},${y})`
-	})
-
-dataPoints
-	.append("text")
-	.attr("x", 175)
-	.attr("y", 100)
-	.attr("class", "country")
-	.text((d, i) => {return d.country})
-	.attr("transform", (d, i) => {
-	const x = (i )
-	const y = Math.floor(i *2) * 120	
-	return `translate(${x},${y})`
-	})
-
-
-const months= dataPoints
-	.append("g")
-	.attr("class", "months")
-	.attr("transform", "translate(200, 0)")
+const colorScale= d3.scaleLinear()
+	.domain([-10, 0, 7, 14, 21, 24])
+	.range(["#814ee7","#3f24ec", "#79e87c", "#fbe157", "#ff9737", "#fe3b3b"])
 	
-const monthGroups= months
-	.selectAll("g.month")
-	.data((d,i) => {return d.months})
-	.enter()
-	.append("g")
-	.attr("class", "month")
-	.attr("transform", (d, i) => {return `translate(${i *50},0)`})
+
+
+const dataPoints = svg
+  .selectAll('g.data-point')
+  .data(data)
+  .enter()
+  .append('g')
+  .attr('class', 'data-point')
+  .attr('transform', (d, i) => {
+    return `translate(0, ${i * 150})`
+  })
+
+dataPoints
+  .append('text')
+  .attr('x', 175)
+  .attr('y', 70)
+  .attr('class', 'city')
+  .text((d, i) => {
+    return d.city
+  })
+ 
+
+dataPoints
+  .append('text')
+  .attr('x', 175)
+  .attr('y', 100)
+  .attr('class', 'country')
+  .text((d, i) => {
+    return d.country
+  })
+ 
+
+const months = dataPoints
+	.append('g')
+	.attr('class', 'months')
+	.attr('transform', 'translate(200, 0)')
+
+const monthGroups = months
+  .selectAll('g.month')
+  .data((d, i) => {return d.months })
+  .enter()
+  .append('g')
+  .attr('class', 'month')
+  .attr('transform', (d, i) => {
+    return `translate(${i * 50},0)`
+  })
 
 monthGroups
-		.append("rect")
-	.attr("x", 0)
-	.attr("y", 0)
-	.attr("width", 40)
-	.attr("height", 140)
-	.style("fill", "red")
-	
+  	.append('rect')
+  	.attr('x', 0)
+  	.attr('y', 0)
+  	.attr('width', 50)
+  	.attr('height', 150)
+	.style("fill", (d,i) => {return colorScale(d)})
+
+monthGroups
+	.append("circle")
+	.attr("cx", 25)
+	.attr("cy", 75)
+	.attr("r", 15)
+
+
+monthGroups
+	.append("text")
+	.attr("class", "temp")
+	.attr("x", 25)
+	.attr("y", 75)
+	.text((d,i) =>{return d})
+	.style("fill", (d,i) => {return colorScale(d)})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

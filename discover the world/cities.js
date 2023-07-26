dca79cf4c7a960d66de10ced6f692c702781eeb4
aroberge66 <alecroberge6@gmail.com>
2023-07-26 16:12:39 -0600

    hi

--- discover the world/cities.js
+++ discover the world/cities.js
@@ -13,8 +13,67 @@ svg
 
 const placeCities= function () {
 	//this is upadated funtions
-	console.log("hi there")
+	
+	let inputX = document.querySelector("select[name=valueX]")
+	let inputY = document.querySelector("select[name=valueY]")
+	
+	
+	let valueX= inputX.value
+	let valueY= inputY.value
+	
+	// max value from the data
+	let maxValueX= d3.max(data, (d,i) =>{ return d[valueX]})
+	let maxValueY= d3.max(data, (d,i) =>{ return d[valueY]})
+	let maxValueR= d3.max(data, (d,i)=> { return d.population})
+	
+	const scaleX = d3.scaleLinear()
+		.domain([0, maxValueX])
+		.range ([100, 860])
+	
+	const scaleY = d3.scaleLinear()
+		.domain([0, maxValueY])
+		.range([620, 100])
+	
+	const scaleR = d3.scaleSqrt()
+		.domain([0, maxValueR])
+		.range([0,30])
+	
+	
+	
+	const cities =svg	
+		.selectAll("g.city")
+		.data(data)
+		.enter()
+		.append("g")
+		.attr("class", "city")
+		.attr("transform", (d, i) => {
+			const x= scaleX(d[valueX])
+			const y=scaleY(d[valueY])
+			return `translate(${x},${y})`
+		})
+	
+	cities
+		.append("circle")
+		.attr("cx", 0)
+		.attr("cy", 0)
+		.attr("r", 0)
+		.transition()
+		.attr("r", (d,i) =>{return scaleR(d.population)})
+	
+	svg
+		.selectAll("g.city")
+		.transition()
+		.duration(500)
+		.attr("transform", (d, i) => {
+			const x= scaleX(d[valueX])
+			const y=scaleY(d[valueY])
+			return `translate(${x},${y})`
+		})
+	
+	
+	
 }
+
 
 
 //on load
@@ -29,6 +88,20 @@ selectTags.forEach((selectTag) => {
 		placeCities()
 	})
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

a76947f601a8a37343f6168f10f5233972c848fa
aroberge66 <alecroberge6@gmail.com>
2023-07-26 12:02:55 -0600

    1

--- discover the world/data-collector.js
+++ discover the world/data-collector.js
@@ -5,14 +5,15 @@ const clean = function (input) {
 }
 
 const apartment = clean(document.querySelector("body > div.innerWidth > table > tbody > tr:nth-child(56) > td.priceValue"))
-const utilities = clean(document.querySelector("body > div.innerWidth > table > tbody > tr:nth-child(40) > td.priceValue"))
-const monthlyPass = clean(document.querySelector("body > div.innerWidth > table > tbody > tr:nth-child(32) > td.priceValue.tr_highlighted"))
-const expensiveMeal = clean(document.querySelector("body > div.innerWidth > table > tbody > tr:nth-child(3) > td.priceValue.tr_highlighted"))
+const utilities = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(40) > td.priceValue > span"))
+const expensiveMeal = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(3) > td.priceValue.tr_highlighted > span"))
 
 const singlePerson = clean(document.querySelector("body > div.innerWidth > div.seeding-call.limit_size_ad_right.padding_lower.other_highlight_color > ul > li:nth-child(2) > span"))
 const family = clean(document.querySelector("body > div.innerWidth > div.seeding-call.limit_size_ad_right.padding_lower.other_highlight_color > ul > li:nth-child(1) > span"))
 
-const city = document.querySelector("body > div.innerWidth > div.seeding-call.limit_size_ad_right.padding_lower.other_highlight_color > p > span").innerHTML
+
+const cappuccino=clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(7) > td.priceValue.tr_highlighted > span"))
+
 
 copy({
   city,
@@ -21,5 +22,6 @@ copy({
   apartment,
   utilities,
   monthlyPass,
-  expensiveMeal
+  expensiveMeal,
+	cappuccino
 })

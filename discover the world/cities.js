ad60386797e29f7b6a09a62eac66fea4f51a9305
aroberge66 <alecroberge6@gmail.com>
2023-07-26 11:04:31 -0600

    start

---
+++ discover the world/cities.js
@@ -0,0 +1,41 @@
+// JavaScript Document
+//data vis on:
+//1. load
+//2. on change to first box
+//3. on change to second box
+//all will run the same code
+
+const svg=d3.select("svg")
+
+svg
+	.attr("width", 960)
+	.attr("height", 720)
+
+const placeCities= function () {
+	console.log("hi there")
+}
+
+
+//on load
+placeCities()
+
+//select all delect boxes
+
+const selectTags=document.querySelectorAll ("select")
+
+selectTags.forEach((selectTag) => {
+	selectTag.addEventListener("change", function () {
+		placeCities()
+	})
+})
+
+
+
+
+
+
+
+
+
+
+

afd8778095bf603ac876490ef373d99a7121341d
aroberge66 <alecroberge6@gmail.com>
2023-07-27 11:30:08 -0600

    finished

--- discover the world/cities.js
+++ discover the world/cities.js
@@ -8,8 +8,35 @@
 const svg=d3.select("svg")
 //this is setup functions
 svg
-	.attr("width", 960)
-	.attr("height", 720)
+	.attr("viewBox", "0 0 960 720")
+	
+
+
+const axisXGroup= svg
+	.append("g")
+	.attr("class", "x-axis")
+	.attr("transform", "translate(0, 622)")
+
+const axisXText= svg
+	.append("text")
+	.attr("class", "x-axis")
+	.attr("transform", "translate(480,690)")
+	.text("axis")
+
+const axisYGroup= svg
+	.append("g")
+	.attr("class", "y-axis")
+.attr("transform", "translate(100, 2)")
+	
+const axisYText= svg
+	.append("text")
+	.attr("class", "y-axis")
+	.attr("transform", "translate(12,360) rotate(-90)")
+	.text("axis")
+
+
+
+
 
 const placeCities= function () {
 	//this is upadated funtions
@@ -21,11 +48,20 @@ const placeCities= function () {
 	let valueX= inputX.value
 	let valueY= inputY.value
 	
+	//axis text/label
+	let textX=inputX.options[inputX.selectedIndex].innerHTML
+	let textY=inputY.options[inputY.selectedIndex].innerHTML
+	
+	axisXText.text(textX)
+	axisYText.text(textY)
+	
+	
 	// max value from the data
 	let maxValueX= d3.max(data, (d,i) =>{ return d[valueX]})
 	let maxValueY= d3.max(data, (d,i) =>{ return d[valueY]})
 	let maxValueR= d3.max(data, (d,i)=> { return d.population})
 	
+	//scales
 	const scaleX = d3.scaleLinear()
 		.domain([0, maxValueX])
 		.range ([100, 860])
@@ -36,13 +72,33 @@ const placeCities= function () {
 	
 	const scaleR = d3.scaleSqrt()
 		.domain([0, maxValueR])
-		.range([0,30])
+		.range([0,40])
+	
+	
+	//axis and graph
+	const axisX = d3.axisBottom(scaleX)
+		.tickSizeInner(-520)
+		.tickSizeOuter(0)
+		.tickPadding(20)
+		.ticks(15, "$,f")
+	
+		
+	const axisY = d3.axisLeft(scaleY)
+		.tickSizeInner(-800)
+		.tickSizeOuter(0)
+		.tickPadding(20)
+		.ticks(15, "$,f")
+	
+
+	axisXGroup.call(axisX)
+	axisYGroup.call(axisY)
 	
 	
 	
+	//inserting data
 	const cities =svg	
 		.selectAll("g.city")
-		.data(data)
+		.data(data, (d,i)=>{return d.city})
 		.enter()
 		.append("g")
 		.attr("class", "city")
@@ -58,7 +114,7 @@ const placeCities= function () {
 		.attr("cy", 0)
 		.attr("r", 0)
 		.transition()
-		.attr("r", (d,i) =>{return scaleR(d.population)})
+		.attr("r", (d,i) =>{return scaleR(d.population)+5})
 	
 	svg
 		.selectAll("g.city")
@@ -70,6 +126,31 @@ const placeCities= function () {
 			return `translate(${x},${y})`
 		})
 	
+	cities
+		.append("rect")
+		.attr("x", -60)
+		.attr("y", (d,i) =>{return -1* scaleR(d.population)-35})
+		.attr("width", 120)
+		.attr("height", 30)
+		.attr("trasnform", (d,i) => { 
+			const x= scaleX(d[valueX])
+			const y=scaleY(d[valueY])
+			return `translate(${x},${y})`
+		})
+	
+	cities
+		.append("text")
+		.attr("x", 0)
+		.attr("y", (d,i) =>{return -1* scaleR(d.population)-15})
+		.text((d,i) => {return d.city})
+	
+	svg
+		.selectAll("g.city")
+		.on("mouseover", function (){
+			d3.select(this).raise()
+		
+	})
+
 	
 	
 }
@@ -78,6 +159,7 @@ const placeCities= function () {
 
 //on load
 placeCities()
+	
 
 //select all delect boxes
 
--- discover the world/data-collector.js
+++ discover the world/data-collector.js
@@ -2,18 +2,42 @@
 
 const clean = function (input) {
   return parseFloat(input.innerHTML.replace(/[$,]/g, ""), 10)
+	
 }
 
 const apartment1 = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(56) > td.priceValue > span"))
-const apartment3 = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(58) > td.priceValue > span"))
-const utilities = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(40) > td.priceValue > span")) + clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(41) > td.priceValue.tr_highlighted > span")) +clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(42) > td.priceValue > span"))
-const expensiveMeal = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(3) > td.priceValue.tr_highlighted > span")) + clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(42) > td.priceValue > span"))
 
+const apartment3 = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(58) > td.priceValue > span"))
+
+
+const basicU = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(40) > td.priceValue > span")) 
+
+const mobile = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(41) > td.priceValue.tr_highlighted > span")) 
+
+const internet = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(42) > td.priceValue > span"))
+
+const utilities = basicU + mobile + internet
+	  
+	  
+const expensiveMeal = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(3) > td.priceValue.tr_highlighted > span"))
 const singlePerson = clean(document.querySelector("body > div.innerWidth > div.seeding-call.limit_size_ad_right.padding_lower.other_highlight_color > ul > li:nth-child(2) > span"))
 const family = clean(document.querySelector("body > div.innerWidth > div.seeding-call.limit_size_ad_right.padding_lower.other_highlight_color > ul > li:nth-child(1) > span"))
 
-
 const cappuccino=clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(7) > td.priceValue.tr_highlighted > span"))
+
+const gas=clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(36) > td.priceValue.tr_highlighted > span"))
+
+const monthlyPass= clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(32) > td.priceValue.tr_highlighted > span"))
+
+const city= document.querySelector("body > div:nth-child(4) > nav.breadcrumb > span:nth-child(6) > a > span")
+
+const bread = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(12) > td.priceValue.tr_highlighted > span"))
+
+const kinder = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(48) > td.priceValue > span"))
+
+const salaryM = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(64) > td.priceValue > span"))
+
+const movie = clean(document.querySelector("body > div:nth-child(4) > table > tbody > tr:nth-child(46) > td.priceValue > span"))
 
 
 copy({
@@ -23,7 +47,18 @@ copy({
   	apartment1,
 	apartment3,
   	utilities,
+	basicU,
+	internet,
+	mobile,
   	monthlyPass,
   	expensiveMeal,
-	cappuccino
+	cappuccino,
+	gas,
+	bread,
+	kinder,
+	salaryM,
+	movie
 })
+
+
+
